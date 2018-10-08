779b4e6a76de29af9e3942f5694fd09d76a9ea02
Watson <watson@watosonnoMacBook-puro.local>
2018-10-08 15:17:05 +0900

    Add new & create action

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -8,8 +8,33 @@ class DiariesController < ApplicationController
     @diaries = Diary.all
   end
 
+  # Add new action
+  def new
+    @diary = Diary.new
+  end
+
   # Add show action
   def show
+  end
+
+  # Add create action
+  def create
+    # 下部で定義している「diary_params」を呼び出し、「_form.html.erb」で入力されたパラメータをハッシュ値として受け取る
+    # 例：内部的にはparams[diary][title]といった２次元配列で持つ
+    @diary = Diary.new(diary_params)
+
+    # 「respond_to」は、アクセスされたURLの末尾によって処理を振り分ける際に使用する。今回は以下の通り。
+    # format.html　→　htmlの場合
+    # format.json　→　jsonの場合
+    respond_to do |format|
+      if @diary.save
+        format.html { redirect_to @diary, notice: 'Diary was successfully created.' }
+        format.json { render :show, status: :created, location: @diary }
+      else
+        format.html { rebnder :new }
+        format.json { render json: @diary.errors, status: :unprocessable_entry }
+      end
+    end
   end
 
   # Add destroy action
@@ -24,6 +49,16 @@ class DiariesController < ApplicationController
   # Add set method
   private def set_diary
     @diary = Diary.find(params[:id])
+  end
+
+  # Add params method
+  def diary_params
+    # 以下のハッシュ値を返す定義
+    # {
+    #   'title': 'XXXX'
+    #   'body': 'XXXX'
+    # }
+    params.require(:diary).permit(:title, :body)
   end
 
 end
