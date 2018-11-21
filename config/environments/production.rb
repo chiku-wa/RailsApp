ad8b49dedc5497e969c08adf3710b727f2b13540
watson <chikuwa.wachi@gmail.com>
2018-11-21 12:36:51 +0900

    [fix]Add devise setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,23 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
+
+  # =====deviseプラグインの設定
+  # デフォルトURL設定
+  config.action_mailer.default_url_options = {host: "localhost", port: 80}
+
+  # SMTPサーバ設定
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => ENV["SMTP_DOMAIN"],
+    :port => ENV["SMTP_PORT"],
+    :domain => ENV["SMTP_DOMAIN"],
+    :user_name => ENV["SMTP_USER_NAME"],
+    :password => ENV["SMTP_PASSWORD"],
+    :authentication => "login",
+  }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -20,7 +35,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -54,7 +69,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -84,9 +99,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
